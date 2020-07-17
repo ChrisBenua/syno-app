d23783a6fbe25c192e8434ce4663e76ad6eba36c
ChrisBenua <kpbenua@edu.hse.ru>
2020-07-17 13:31:58 +0300

    Added animations, headers for collection view if there is none cards or dicts

---
+++ syno-mobile/syno-mobile/Helpers/BackspaceUITextField.swift
@@ -0,0 +1,9 @@
+//
+//  BackspaceUITextField.swift
+//  syno-mobile
+//
+//  Created by Ирина Улитина on 16.07.2020.
+//  Copyright © 2020 Christian Benua. All rights reserved.
+//
+
+import Foundation
---
+++ syno-mobile/syno-mobile/Presentation/Controllers/Views/EmptyDictsCollectionViewHeader.swift
@@ -0,0 +1,9 @@
+//
+//  EmptyDictsCollectionViewHeader.swift
+//  syno-mobile
+//
+//  Created by Ирина Улитина on 17.07.2020.
+//  Copyright © 2020 Christian Benua. All rights reserved.
+//
+
+import Foundation
