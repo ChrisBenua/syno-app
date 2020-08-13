7f1cd0f8db65b268feb82e4e78523d00dbaebed9
ChrisBenua <kpbenua@edu.hse.ru>
2020-08-13 18:04:07 +0300

    increased auth session length removed passwords from DTOs toString

--- syno_backend/src/main/java/com/syno_back/backend/BackendApplication.java
+++ syno_backend/src/main/java/com/syno_back/backend/BackendApplication.java
@@ -2,6 +2,7 @@ package com.syno_back.backend;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.security.core.context.SecurityContextHolder;
 
 /**
  * Main runner class for entire application
@@ -10,6 +11,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class BackendApplication {
 
     public static void main(String[] args) {
+        SecurityContextHolder.clearContext();
         SpringApplication.run(BackendApplication.class, args);
     }
 
